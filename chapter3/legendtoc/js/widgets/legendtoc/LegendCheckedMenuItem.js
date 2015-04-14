7ba1b9159ad488e5ddf5cdb9f0aaa6434ca2b1cf
odoe <odoe@odoe.net>
2015-04-14 09:07:05 -0700

    fixed some formatting

--- chapter3/legendtoc/js/widgets/legendtoc/LegendCheckedMenuItem.js
+++ chapter3/legendtoc/js/widgets/legendtoc/LegendCheckedMenuItem.js
@@ -1,18 +1,18 @@
-    define([
-        'dojo/_base/declare',
-        'dijit/CheckedMenuItem',
-        'text!widgets/legendtoc/templates/LegendCheckedMenuItem.tpl.html',
-        'dijit/hccss'
-        ], function(declare, CheckedMenuItem, template) {
-            /**
-             * Extends dijit.CheckedMenuItem.
-             * Requires a customized template html file.
-             * @constructor
-             */
-            var LegendCheckedMenuItem = declare([CheckedMenuItem], {
-                templateString: template,
-                legendUrl: ''
-            });
+define([
+  'dojo/_base/declare',
+  'dijit/CheckedMenuItem',
+  'text!widgets/legendtoc/templates/LegendCheckedMenuItem.tpl.html',
+  'dijit/hccss'
+], function(declare, CheckedMenuItem, template) {
+    /**
+      * Extends dijit.CheckedMenuItem.
+      * Requires a customized template html file.
+      * @constructor
+      */
+    var LegendCheckedMenuItem = declare([CheckedMenuItem], {
+        templateString: template,
+        legendUrl: ''
+    });
 
-            return LegendCheckedMenuItem;
-        });
+    return LegendCheckedMenuItem;
+});
--- chapter3/legendtoc/js/widgets/legendtoc/LegendMenuItem.js
+++ chapter3/legendtoc/js/widgets/legendtoc/LegendMenuItem.js
@@ -1,21 +1,21 @@
-    define([
-        'dojo/_base/declare',
-        'dijit/MenuItem',
-        'text!widgets/legendtoc/templates/LegendMenuItem.tpl.html',
-        'dijit/hccss'
-        ], function(declare, MenuItem, template) {
+define([
+  'dojo/_base/declare',
+  'dijit/MenuItem',
+  'text!widgets/legendtoc/templates/LegendMenuItem.tpl.html',
+  'dijit/hccss'
+], function(declare, MenuItem, template) {
 
-            //http://bugs.dojotoolkit.org/ticket/16177
+    //http://bugs.dojotoolkit.org/ticket/16177
 
-            /**
-             * Extends dijit.MenuItem for use in LegendToc
-             * @constructor
-             */
-            var LegendMenuItem = declare([MenuItem], {
-                templateString: template,
-                legendUrl: ''
-            });
+    /**
+      * Extends dijit.MenuItem for use in LegendToc
+      * @constructor
+      */
+    var LegendMenuItem = declare([MenuItem], {
+        templateString: template,
+        legendUrl: ''
+    });
 
-            return LegendMenuItem;
+    return LegendMenuItem;
 
-        });
+});
